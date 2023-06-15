66bc708699723015aba1e7a01c9629bdd77b57a0
Pedro Requiao <alonsofts@gmail.com>
2023-06-15 18:31:21 -0400

    Finishing Success Page and integration with Stripe. This would be 1.0 release

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { HomeContainer, Product } from "@/styles/pages/home"
+import Head from 'next/head'
 import Image from "next/image"
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
@@ -26,23 +27,27 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
+    <>
+      <Head>
+        <title>Home | IgShop</title>
+      </Head>
 
-      {products.map(product => {
-        return (
-          <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
-            <Product className="keen-slider__slide">
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>)
-      })}
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map(product => {
+          return (
+            <Link key={product.id} href={`/product/${product.id}`} prefetch={false}>
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>)
+        })}
+      </HomeContainer>
+    </>
 
-
-    </HomeContainer>
   )
 }
 
